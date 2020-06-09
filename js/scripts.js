6d164d3e3bb59995dec1d12e77b206d6cfe3fdf5
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-09 15:16:15 -0700

    add html boiler plate, add check box form to gather user input, add submit function and .each function to js, add bootstrap and jQuery to css and js folders.
    Co-authored-by: Matt Macfarland<mrmcfarland@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//business logic
+$(document).ready(function(){
+  $("form#stress-test").submit(function(event) {
+    event.preventDefault();
+    $("#stress-test").show();
+    $("input:checkbox[name=stress-symptoms]:checked").each(function(){
+      const stressResponses = $(this).val();
+      $('#stress-responses').append(stressResponses + "<br>");
+    });
+    
+  });
+});
